515d40a37d6ac7ffd3f4348b30c4e8209edd5d44
CamilaGomez1996 <126061033+CamilaGomez1996@users.noreply.github.com>
2023-03-30 17:00:02 -0300

    inicial commit

---
+++ cv.js/cv.js
@@ -0,0 +1,23 @@
+/**Este ejemplo me da mucha alegria porque despues de mucho esfuerzo y prueba logre que algo funcione
+ * console.log(10 + 10)
+ */
+
+function  myFunction() {
+    ("carta")
+    document.getElementById("demo1").innerHTML = "A quien pueda interesar:";
+    document.getElementById("demo2").innerHTML = "Reciba un cordial y respetuoso saludo. A través de estas líneas deseo hacer de su conocimiento mi nombre es LYDIA BAILEY me considero una ciudadana con una conducta intachable. He demostrado ser una excelente y un gran trabajadora, comprometida, responsable y fiel cumplidora de mis tareas. Siempre he manifestado preocupación por mejorar, capacitarme y actualizar mis conocimientos.";
+    document.getElementById("demo3").innerHTML = "Durante estos años me he desempeñado como: OBRERA. Es por ello que, le sugiero considere esta recomendación, con la confianza de que estare siempre a la altura de sus compromisos y responsabilidades.";
+    document.getElementById("demo4").innerHTML = "Sin más nada a que referirme y, esperando que esta misiva sea tomada en cuenta, dejo mi número de contacto para cualquier información de interés.";
+    document.getElementById("demo5").innerHTML = "Atentamente";
+}
+
+function myFunction1() {
+  ("boton_color_color") 
+  document.getElementById("boton_color_color").innerHTML =
+  document.body.style.backgroundColor="white"
+}
+
+function displayDate() {
+    document.getElementById("fecha").innerHTML = Date();
+  }
+
